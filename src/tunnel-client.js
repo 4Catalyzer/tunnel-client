d5431803ed95caf1e6bb047bb628a82f6de646d5
Giacomo Tagliabue <giacomo.tag@gmail.com>
2016-01-11 11:53:43 -0500

    update reverse tunnel dependency

--- src/tunnel-client.js
+++ src/tunnel-client.js
@@ -1,11 +1,10 @@
 /* eslint new-cap: 0 */
-import wts from 'node-reverse-wstunnel';
+import { reverseClient } from 'reverse-wstunnel';
 
 /**
  * open a reverse tunnel on `serverUrl` from port `tunnelPort` pointing to
  * `netloc`
  */
 export function open(tunnelPort, serverUrl, netloc, headers) {
-  const reverseClient = new wts.client_reverse();
-  reverseClient.start(tunnelPort, serverUrl, netloc, headers);
+  new reverseClient().start(tunnelPort, serverUrl, netloc, headers);
 }
